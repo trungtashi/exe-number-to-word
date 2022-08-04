f7e67e1bed3cb5836dd83e54a0b8fb528f487112
trungtashi99 <trungtashi@gmai.com>
2022-08-04 17:03:02 +0700

    Bài tập đổi từ số sang chữ

---
+++ src/number_to_word.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class number_to_word {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        String word = oneNumber(number);
+        System.out.println(word);
+
+    }
+    public static String oneNumber(int number){
+
+            if(number < 10){
+                switch (number){
+                    case 0:
+                        System.out.println("Zero");
+                        break;
+                    case 1:
+                        System.out.println("One");
+                        break;
+                    case 2:
+                        System.out.println("Two");
+                        break;
+                    case 3:
+                        System.out.println("Three");
+                        break;
+                    case 4:
+                        System.out.println("Four");
+                        break;
+                    case 5:
+                        System.out.println("Five");
+                        break;
+                    case 6:
+                        System.out.println("Six");
+                        break;
+                    case 7:
+                        System.out.println("Seven");
+                        break;
+                    case 8:
+                        System.out.println("Eight");
+                        break;
+                    case 9:
+                        System.out.println("Nine");
+                        break;
+                    default:
+                        return "out of ability";
+                }
+            } else if (number < 20) {
+                switch (number){
+                    case 0:
+                        System.out.println("Ten");
+                        break;
+                    case 1:
+                        System.out.println("Eleven");
+                        break;
+                    case 2:
+                        System.out.println("Two");
+                        break;
+                    case 3:
+                        System.out.println("Three");
+                        break;
+                    case 4:
+                        System.out.println("Four");
+                        break;
+                    case 5:
+                        System.out.println("Five");
+                        break;
+                    case 6:
+                        System.out.println("Six");
+                        break;
+                    case 7:
+                        System.out.println("Seven");
+                        break;
+                    case 8:
+                        System.out.println("Eight");
+                        break;
+                    case 9:
+                        System.out.println("Nine");
+                        break;
+                    default:
+                        return "out of ability";
+                }
+            }
+        return null;
+    }
+}
